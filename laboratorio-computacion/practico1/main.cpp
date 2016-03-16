bc0c4ebac02f869544b9a86dbc11e66f94d405cf
Arcangel Andres <artigue_arcangel@hotmail.com>
2016-03-16 01:39:29 -0300

    Se eliminan archivos innecesarios y se modifica el main.cpp

--- laboratorio-computacion/practico1/main.cpp
+++ laboratorio-computacion/practico1/main.cpp
@@ -1,10 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main () {
+	/* variables */
+	int p,q,t;
+	float s;
+	char car;
+	
 	printf ("Hoy es lunes \n");
 	printf ("Arcangel ");
 	printf ("Andres ");
-	printf ("Artigue");
+	printf ("Artigue \n");
+	printf ("==============================\n");
+	printf ("Ingrese un valor:");
+	scanf ("%i",&p);
+	printf ("Ingrese un valor:");
+	scanf ("%i",&q);
+	printf ("==============================\n");	
+	printf ("El valor es %i \n", p);
+	printf ("==============================\n");
+	printf ("El valor es %i \n", q);
+	printf ("==============================\n");
+	t = p+q;
+	printf ("El valor de la suma es: %i \n",t);
+	s = t*0.5;
+	printf ("Porcentaje: %f \n",s);
+	printf ("Ingrese un caracter: ");
+	scanf ("%c", &car);
+	printf ("El valor es: %c \n",car);
+	
 	return 0;
 }
 
