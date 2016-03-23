ce62f4ee4c94aab96a767b642825e733be1a4205
Arcangel Andres <artigue_arcangel@hotmail.com>
2016-03-23 16:05:00 -0300

    Hola mundo en java

---
+++ programacion-1/HolaMundo.java
@@ -0,0 +1,11 @@
+/**	
+ * @author Artigue Arcangel Andres <artigue_arcangel@hotmail.com>
+ * @version 2016.03.23
+ *
+ */
+
+public class HolaMundo {
+	public static void main (String [] args) {
+		System.out.println("Hola Mundo!!");
+	}
+}
