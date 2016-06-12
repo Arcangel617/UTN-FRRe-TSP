e47db2189b94c0953782eab6f95b07cc0df6a067
Arcangel Andres <artigue_arcangel@hotmail.com>
2016-06-12 00:54:23 -0300

    Se corrigen algunos comentarios  y estructuras

--- laboratorio-computacion/teoria/arreglos1.c
+++ laboratorio-computacion/teoria/arreglos1.c
@@ -7,31 +7,47 @@ int main()
 	int min=10000;
 	int max=-10000;
 	int a = 0;
-	int promedio;
+	float promedio;
 	
-	/*Cargar elementos en el arreglo*/
+	/*
+	* Carga de elementos en el arreglo mediante una estructura repetitiva.
+	*/
 	for(i = 0; i < 10; i++)
 	{
-		printf("Ingresar el elemento %d:\n",i);
+		printf("Ingresar el elemento [%d]: ",i);
 		scanf("%d",&lista[i]);
 	}
-	/*Mostrar los elementos del arreglo*/
-	for(i = 0; i < 10; i++)
-		printf("Digito %d:%d\n",i,lista[i]);
 	
+	/*
+	* Se muestra los elementos del arreglo con una estructura repetitiva.
+	*/
+	for(i = 0; i < 10; i++)
+		printf("Digito [%d]: %d\n",i,lista[i]);
+	
+	/*
+	* Se muestran los numeros pares, el menor, el mayor y el promedio de la
+	* suma de los elementos.
+	*/
 	for (i=0; i<10; i++)
 	{
-		if (lista[i]%2==0) printf("\u201cDigito par: %d\n\u201d", lista[i]);
-		if (lista[i]<min) min=lista[i];
-		else if (lista[i]>max) max=lista[i];
+		if (lista[i]%2==0){ 
+			printf("Digito par: %d\n", lista[i]);
+		}
+		if (lista[i]<min){
+			min=lista[i];
+		}
+		else if (lista[i]>max){ 
+			max=lista[i];
+		}
 	}
-	printf ("\u201cel numero mayor es: %d y el numero menor es: %d\u201d",max,min);
+	printf ("El numero mayor es: %d y el numero menor es: %d\n",max,min);
+	
 	for(i=0;i<10;i++)
 	{
 		a=a+lista[i];
 	}
 	promedio=a/i;
-	printf("\u201c\n el promedio es %d\u201d",promedio);	
+	printf("El promedio es %f",promedio);	
 	
 	return 0;
 }
